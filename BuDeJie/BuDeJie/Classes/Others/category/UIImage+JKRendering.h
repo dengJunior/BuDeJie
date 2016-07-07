74ff52652227d6d1ad064f68e9cd2920dd92c7b3
JokerHjh <joker_he@outlook.com>
2016-07-07 22:30:29 +0800

    给分类方法添加了文档注释

--- BuDeJie/BuDeJie/Classes/Others/category/UIImage+JKRendering.h
+++ BuDeJie/BuDeJie/Classes/Others/category/UIImage+JKRendering.h
@@ -10,6 +10,21 @@
 
 @interface UIImage (JKRendering)
 
+/**
+ *  根据图片名和渲染方式生成一个UIImage对象
+ *
+ *  @param imageName 图片名
+ *  @param mode      渲染方式
+ *
+ *  @code
+ *  渲染方式有：
+ *  UIImageRenderingModeAutomatic,        自动渲染
+ *  UIImageRenderingModeAlwaysOriginal,   不渲染，返回原图
+ *  UIImageRenderingModeAlwaysTemplate,   按模板渲染
+ *  @endcode
+ *
+ *  @return 返回一个UIImage对象
+ */
 + (UIImage *)imageNamed:(NSString *)imageName WithRendingMode:(UIImageRenderingMode)mode;
 
 @end
