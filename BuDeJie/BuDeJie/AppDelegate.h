0f66eebc3203340540cb2bb9561bca4848ba0147
JokerHjh <joker_he@outlook.com>
2016-07-07 20:10:20 +0800

    初始化

---
+++ BuDeJie/BuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuDeJie
+//
+//  Created by Joker on 16/7/7.
+//  Copyright © 2016年 Joker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
