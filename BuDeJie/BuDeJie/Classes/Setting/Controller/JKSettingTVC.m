76b9c4524ea95fa821d136675cfc6416562fe9c3
JokerHjh <joker_he@outlook.com>
2016-07-14 20:49:16 +0800

    计算缓存大小

--- BuDeJie/BuDeJie/Classes/Setting/Controller/JKSettingTVC.m
+++ BuDeJie/BuDeJie/Classes/Setting/Controller/JKSettingTVC.m
@@ -7,8 +7,23 @@
 //
 
 #import "JKSettingTVC.h"
+#import <SDImageCache.h>
+
+static NSString *settingCellID = @"settingCellID";
 
 @implementation JKSettingTVC
+
+#pragma mark -
+#pragma mark view cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // 设置本页标题
+    self.title = @"设置";
+    
+    // 注册cell重用标识
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:settingCellID];
+}
 
 #pragma mark -
 #pragma mark tableView dataSource
@@ -17,10 +32,16 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *settingCellID = @"settingCellID";
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:settingCellID];
+    
+    // SDWebImage计算的缓存大小
+    NSInteger sizeSD = [[SDImageCache sharedImageCache] getSize];
+    JKLog(@"sizeSD--%ld", sizeSD)
+    
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:settingCellID forIndexPath:indexPath];
     
     cell.textLabel.text = @"清除缓存";
+    
     return cell;
 }
 
@@ -41,6 +62,37 @@
 #pragma mark 对缓存的操作
 - (void)cleanCaches {
     // 计算缓存大小
+    NSString *cachePath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
+    // 创建文件管理者对象
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    // 总大小
+    NSInteger totalSize = 0;
+    // 取缓存路径
+    cachePath = [cachePath stringByAppendingString:@"/default"];
+    // 遍历路径下所有文件计算大小
+    for (NSString *subPath in [fileManager subpathsOfDirectoryAtPath:cachePath error:nil]) {
+        // 拼接全路径
+        NSString *fullPath = [cachePath stringByAppendingPathComponent:subPath];
+        
+        BOOL isDirectory; // 是否是文件夹
+        BOOL isExist = [fileManager fileExistsAtPath:fullPath isDirectory:&isDirectory]; // 是否存在
+        // 如果路径不存在，或路径是文件夹
+        if (isExist == NO || isDirectory) continue;
+        // 排除系统文件
+        if ([fullPath containsString:@".DS_Store"]) continue;
+        
+        // 取文件属性
+        NSDictionary *attrs = [fileManager attributesOfItemAtPath:fullPath error:nil];
+        
+        // 文件大小
+        NSNumber *size = (NSNumber *)attrs[NSFileSize];
+        // 累加
+        totalSize += size.integerValue;
+    }
+    JKLog(@"totalSize = %ld", totalSize)
+//    NSDictionary *attr = [fileManager attributesOfItemAtPath:cachePath error:nil];
+//    JKLog(@"attr---%@", attr)
+    
     
     // 清除
     
