ca9b802bb264282bb226c609646b8dffba0996f9
JokerHjh <joker_he@outlook.com>
2016-07-15 20:09:22 +0800

    抗锯齿处理（效果不是很理想，不如用cornerRadius）

--- BuDeJie/BuDeJie/Classes/Others/category/UIImage+JKRendering.m
+++ BuDeJie/BuDeJie/Classes/Others/category/UIImage+JKRendering.m
@@ -17,20 +17,43 @@
 }
 
 + (UIImage *)circleImageWithOriginalImage:(UIImage *)oriImage {
+    
+    UIImage *newImage = [UIImage antialiasImageWithOriginImage:oriImage];
+    
+    // 预留边宽（防锯齿）
+//    CGFloat border = 1;
+    // 上下文大小
+//    CGSize size = CGSizeMake(oriImage.size.width + border *2, oriImage.size.height + border *2);
     // 开启上下文
-    UIGraphicsBeginImageContextWithOptions(oriImage.size, NO, 0);
+    UIGraphicsBeginImageContextWithOptions(newImage.size, NO, 0);
     // 绘制裁剪范围
-    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, oriImage.size.width, oriImage.size.height)];
+    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, newImage.size.width, newImage.size.height)];
     // 裁剪
     [path addClip];
     // 绘图
-    [oriImage drawAtPoint:CGPointZero];
+    [oriImage drawAtPoint:CGPointMake(0, 0)];
     // 从上下文取出图片
     UIImage *circleImage = UIGraphicsGetImageFromCurrentImageContext();
     // 关闭上下文
     UIGraphicsEndImageContext();
     
     return circleImage;
+}
+
++ (UIImage *)antialiasImageWithOriginImage:(UIImage *)oriImage {
+    
+    CGSize smallSize = CGSizeMake(oriImage.size.width -2, oriImage.size.height -2);
+    UIGraphicsBeginImageContextWithOptions(smallSize, NO, 0);
+    [oriImage drawInRect:CGRectMake(-1, -1, oriImage.size.width, oriImage.size.height)];
+    UIImage *smallImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    UIGraphicsBeginImageContextWithOptions(oriImage.size, NO, 0);
+    [smallImage drawInRect:CGRectMake(1, 1, smallSize.width, smallSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
 }
 
 @end
